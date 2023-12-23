e1232eb49e3f032b670ab5d228a705f17e0e5f4c
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2023-12-23 18:23:10 +0530

    Change Register and Login components

--- Frontend/src/components/User Management/User Registration/Register.js
+++ Frontend/src/components/User Management/User Registration/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../../../API/User_API';
+import './Register.css';
 
 function Register() {
   const navigate = useNavigate();
@@ -43,35 +44,52 @@ function Register() {
   };
 
   return (
-    <div>
-      <h2>Registration</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+    <div className='Register' fluid>
+      <div className='RegisterCard'>
+        <div className='RegisterDiv1'>
+          <img src="/Images/Register.jpg" alt='register'/>
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
+        <div className='RegisterDiv2'>
+          <div className='RegisterHeader'>
+            <h2>Create an Account</h2>
+          </div>
+          <div className='RegisterForm'>
+            <form onSubmit={handleSubmit} className='Register_form'>
+              <div>
+                <label htmlFor="name">Full Name :</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="email">Email :</label>
+                <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="phone">Phone Number :</label>
+                <input type="number" id="phone" name="phone" value={formData.phone} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="nic">NIC :</label>
+                <input type="text" id="nic" name="nic" value={formData.nic} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="age">Date of Birth :</label>
+                <input type="text" id="dob" name="dob" value={formData.dob} onChange={handleChange}/>
+              </div>
+              <div>
+                <label htmlFor="password">Password :</label>
+                <input type="text" id="password" name="password" value={formData.password} onChange={handleChange}/>
+              </div>
+              {/* <div>
+                <label htmlFor="password">Confirm Password :</label>
+                <input type="text" id="password" name="password" value={formData.password} onChange={handleChange}/>
+              </div> */}
+            </form>
+          </div>
+          <div className='RegisterButton'>
+            <button type="submit">Create</button>
+          </div>
         </div>
-        <div>
-          <label htmlFor="phone">Phone:</label>
-          <input type="number" id="phone" name="phone" value={formData.phone} onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="nic">NIC:</label>
-          <input type="text" id="nic" name="nic" value={formData.nic} onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="age">Date of Birth:</label>
-          <input type="date" id="dob" name="dob" value={formData.dob} onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="text" id="password" name="password" value={formData.password} onChange={handleChange}/>
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      </div>
     </div>
   );
 }
