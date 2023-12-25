d84be37d79e725fc1718bb0b405936d21d0da761
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2023-12-25 23:32:57 +0530

    Change Dashboard

--- Frontend/src/Pages/UserDashboard.js
+++ Frontend/src/Pages/UserDashboard.js
@@ -9,16 +9,16 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from 'cdbreact';
+// import { NavLink } from 'react-router-dom';
+// import {
+//   CDBSidebar,
+//   CDBSidebarContent,
+//   CDBSidebarFooter,
+//   CDBSidebarHeader,
+//   CDBSidebarMenu,
+//   CDBSidebarMenuItem,
+// } from 'cdbreact';
 
 import { getRecipes } from '../API/Recipe_API';
 import { getRecipe } from '../API/Recipe_API';
@@ -26,6 +26,7 @@ import { getRecipe } from '../API/Recipe_API';
 function UserDashboard() {
     const { state } = useLocation();
     const user = state?.user;
+    const chef = user.user;
     const navigate = useNavigate();
 
     const [recipes, setRecipes] = useState([]);
@@ -50,58 +51,56 @@ function UserDashboard() {
       fetchRecipes();
     }, []);
 
-    console.log("recipe",recipes);
-
     const handleProfile = () => {
         // Navigate to the Profile route
         navigate('/UserDashboard/Profile', { state: { user: user } });
     };
 
-    const handleAddRecipe = () => {
-      // Navigate to the Profile route
-      navigate('/UserDashboard/AddRecipe', { state: { user: user } });
+    const handleLogout = () => {
+      // Navigate to the login route
+      navigate('/Login');
     };
 
     const handleRecipeLink = async (recipe) => {
 
       try {
         const response = await getRecipe(recipe);
-        console.log("response:", response);
-
+        
         // Navigate to the recipe route
-        navigate('/UserDashboard/ViewRecipe', { state: { recipe: recipe } });
+        navigate('/UserDashboard/ViewRecipe', { state: { recipe: recipe, result: response } });
       } catch (error) {
         alert('An error occurred. Please try again..');
       }
     }
 
   return (
-    <Container className="UserDashboard" fluid>
-      <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary UserDashboardNav">
-        <Container>
-          <Navbar.Brand href="#home">Recipe Book</Navbar.Brand>
+    <section className="UserDashboard">
+      <Navbar expand="lg" className="bg-body-tertiary UserDashboardNav">
+        <div className='UserDashboardNavbarTitle'>
+          <Navbar.Brand href="#home" className='UserDashboardNavbarTitleBrand'>Recipe Book</Navbar.Brand>
+        </div>
+        <div className='UserDashboardNavbarLinks'>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className='d-flex justify-content-center'>
             <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link>
-              <Nav.Link href="#about">Category</Nav.Link>
-              <Nav.Link href="#contact">Chefs</Nav.Link>
+              <Nav.Link href="#home" className='UserDashboardNavbarLinksNav'>Home</Nav.Link>
+              <Nav.Link href="#services" className='UserDashboardNavbarLinksNav'>Services</Nav.Link>
+              <Nav.Link href="#about" className='UserDashboardNavbarLinksNav'>Category</Nav.Link>
+              <Nav.Link href="#contact" className='UserDashboardNavbarLinksNav'>Chefs</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-          <div className='UserDashboardButton' >
-            <Button variant="outline-primary" className='UserDashboardButtonProfile' onClick={handleProfile}>Profile</Button>
-            <Button variant="outline-primary" className='UserDashboardButtonAddRecipe' onClick={handleAddRecipe}>Add Recipe</Button>
+        </div>
+        <div className='UserDashboardNavbarButton'>
+          <div className='UserDashboardButton'>
+            <Button className='UserDashboardButtonProfile' onClick={handleProfile}>Pula</Button>
+            <Button className='UserDashboardButtonLogout' onClick={handleLogout}>Log out</Button>
           </div>
-        </Container>
+        </div>
       </Navbar>
       <div className='UserDashboardContent'>
-        <div className='UserDashboardContent'>
-          <CDBSidebar className="UserDashboardSideBar">
+          {/* <CDBSidebar className="UserDashboardSideBar">
             <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-              <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-                Recipe Book
-              </a>
+              <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}> Recipe Book </a>
             </CDBSidebarHeader>
 
             <CDBSidebarContent className="sidebar-content">
@@ -112,7 +111,7 @@ function UserDashboard() {
                 <NavLink exact to="/myRecipes" activeClassName="activeClicked">
                   <CDBSidebarMenuItem icon="table">My Recipes</CDBSidebarMenuItem>
                 </NavLink>
-                <NavLink exact to="/profile" activeClassName="activeClicked">
+                <NavLink onClick={handleProfile} activeClassName="activeClicked">
                   <CDBSidebarMenuItem icon="user">Profile</CDBSidebarMenuItem>
                 </NavLink>
                 <NavLink exact to="/favourites" activeClassName="activeClicked">
@@ -125,34 +124,30 @@ function UserDashboard() {
             </CDBSidebarContent>
             <CDBSidebarFooter style={{ textAlign: 'center' }}>
               <div
-                style={{
-                  padding: '20px 5px',
-                }}
+                style={{ padding: '20px 5px', }}
               >
                 Sidebar Footer
               </div>
             </CDBSidebarFooter>
-          </CDBSidebar>
+          </CDBSidebar> */}
           <Container>
             <Row xs={1} md={2} lg={5} className="g-4 mt-3 UserDashboardCardRow">
               {recipes.map((recipe) => (
                 <Col key={recipe._id}>
                   <Card className='UserDashboardCard' onClick={() => handleRecipeLink(recipe)}>
-                    <Link>
+                    <Link className='UserDashboardCardLink'>
                       <Card.Img variant="top" src={recipe.imgUrl} alt={recipe.title} />
                     </Link>
-                    <Card.Body>
+                    <Card.Body className='UserDashboardCardBody'> 
                       <Card.Title>{recipe.title}</Card.Title>
-                      {/* Add other details like category, etc. if needed */}
                     </Card.Body>
                   </Card>
                 </Col>
               ))}
             </Row>
           </Container>
-        </div>
       </div>
-    </Container>
+    </section>
     
   );
 }
