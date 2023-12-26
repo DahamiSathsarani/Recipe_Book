9edc9e198b47de1c328e526d324b9adb62c586db
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2023-12-26 17:41:02 +0530

    Create My Recipes section in profile

--- Frontend/src/components/User Management/View Profile/Profile.js
+++ Frontend/src/components/User Management/View Profile/Profile.js
@@ -1,40 +1,47 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import Table from 'react-bootstrap/Table';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button';
+import Row from 'react-bootstrap/esm/Row';
+import Container from 'react-bootstrap/esm/Container';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import './Profile.css'
-import { deleteUser } from '../../../API/User_API';
-// import React, { useState, useEffect } from 'react';
+
+import { getSpecificUserRecipes } from '../../../API/Recipe_API';
 
 function Profile() {
   const navigate = useNavigate();
 
   const { state } = useLocation();
   const user = state?.user;
-  console.log(user);
   const chef = user.user;
   
+  const [recipes, setRecipes] = useState([]);
 
-  // const [student, setStudents] = useState({});
-  
-  //   useEffect(() => {
-  //     async function fetchStudents() {
-  //       const studentData = JSON.stringify(state?.user);
-  //       console.log(studentData);
-  //       setStudents(studentData);
-  //     }
-  
-  //     fetchStudents();
-  //   });
-  
-  if (!chef) {
-    return <div>No user data available.</div>;
-  }
+  // Fetch recipes
+  useEffect(() => {
+      
+    const fetchRecipes = async () => {
+      try {
+        const response = await getSpecificUserRecipes(chef.user_id);
+        
+        if (Array.isArray(response)) {
+          setRecipes(response);
+        } else {
+          console.error(`Failed to fetch recipes. ${response.error}`);
+        }
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+      }
+    };
+
+    fetchRecipes();
+  }, [chef.user_id]);
 
   const handleProfile = async (err) => {
     err.preventDefault();
     try {
-        // const response = await login(formData);
-    
         // Navigate to the Update route
         navigate('/UserDashboard/Profile/UpdateProfile', { state: { user: user } });
       } catch (error) {
@@ -42,71 +49,67 @@ function Profile() {
       }
   };
 
-  const handleDelete = async (err) => {
-    err.preventDefault();
-    try {
-        const response = await deleteUser(chef);
-        if (response.success) {
-          // Successfully deleted
-          alert("Successfully Deleted");
-          // Navigate to the Login route
-          navigate('/Login');
+  // const handleDelete = async (err) => {
+  //   err.preventDefault();
+  //   try {
+  //       const response = await deleteUser(chef);
+  //       if (response.success) {
+  //         // Successfully deleted
+  //         alert("Successfully Deleted");
+  //         // Navigate to the Login route
+  //         navigate('/Login');
 
-        } else {
-          alert('Failed deleting. Please try again..');
-        }
+  //       } else {
+  //         alert('Failed deleting. Please try again..');
+  //       }
   
-      } catch (error) {
-        alert('An error occurred. Please try again..');
-      }
-  };
+  //     } catch (error) {
+  //       alert('An error occurred. Please try again..');
+  //     }
+  // };
 
   return (
-    <div>
-      <h2>Your Profile</h2>
-      <Table striped bordered hover>
-      {/* <thead>
-        <tr>
-          <th>User ID</th>
-          <th>User Name</th>
-          <th>NIC</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Age</th>
-        </tr>
-      </thead> */}
-      <tbody>
-        <tr>
-          <th>User Name</th>
-          <td>{chef.name}</td>
-        </tr>
-        <tr>
-          <th>NIC</th>
-          <td>{chef.nic}</td>
-        </tr>
-        <tr>
-          <th>Email</th>
-          <td>{chef.email}</td>
-        </tr>
-        <tr>
-          <th>Phone</th>
-          <td>{chef.phone}</td>
-        </tr>
-        <tr>
-          <th>Date of Birth</th>
-          <td>{chef.dob}</td>
-        </tr>
-        <tr>
-          <th>Password</th>
-          <td>{chef.password}</td>
-        </tr>
-      </tbody>
-    </Table>
-    <div className='button'>
-      <Button onClick={handleProfile}>Edit Profile</Button>
-      <Button onClick={handleDelete}>Delete Profile</Button>
-    </div>
-    </div>
+    <section className='Profile' fluid>
+      <Container className='ProfileHeader'>
+        <Row>
+          <div className='col-md-4 ProfilePersonalDetails'>
+              <Row className='d-flex justify-content-center align-items-center p-3'>
+                <img className='ProfileImage' src={chef.imgUrl} alt=''></img>
+              </Row>
+              <Row className='p-3'>
+                <h1>{chef.name}</h1>
+                <h5>{chef.email}</h5>
+              </Row>
+              <Row className='d-flex justify-content-center p-3'>
+                <Button className='' onClick={handleProfile}>Edit Profile</Button>
+              </Row>
+            </div>
+          <div className='col-md-8'>
+            <Row className='p-3'>
+              <h2>My Recipes</h2>
+            </Row>
+            <Row xs={1} md={2} lg={5} className="g-4 p-3">
+              {recipes.map((recipe) => (
+                <Col key={recipe._id} className='m-3'>
+                  <Card className='ProfileMyrecipesCard'>
+                    <Link className=''>
+                      <Card.Img variant="top" src={recipe.imgUrl} alt={recipe.title} />
+                    </Link>
+                    <Card.Body className=''> 
+                      <Card.Title className='ProfileMyrecipesCardTitle'>{recipe.title}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+            <Row>
+              <Button onClick={handleProfile}>See More</Button>
+            </Row>
+          </div>
+        </Row>
+      </Container>
+      
+    </section>
   );
   }
 
